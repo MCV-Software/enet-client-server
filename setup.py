4a682adc1f9655d36ece88bee70496e7a3839843
Manuel Cortez <manuel@manuelcortez.net>
2022-04-08 15:19:15 -0500

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="enetcomponents", version="0.1.0", author="Manuel cortez", author_email="manuel@manuelcortez.net", url="https://code.manuelcortez.net/manuelcortez/enet-client-server", packages=["enetcomponents"], long_description=open("README.md", "r").read(), description="Enet components to make client-server communication a bit easier.", install_requires=["pyenet"])
+setup(name="enetcomponents", version="0.1.1", author="Manuel cortez", author_email="manuel@manuelcortez.net", url="https://code.manuelcortez.net/manuelcortez/enet-client-server", packages=["enetcomponents"], long_description=open("README.md", "r").read(), description="Enet components to make client-server communication a bit easier.", install_requires=["pyenet"])
